bae2b2398ae31167a321167ab3b53cb99a7f6197
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-08-03 13:25:27 +0200

    Add support for multiple entrypoints in the build tooling

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import fs from "fs"
 import resolve from "@rollup/plugin-node-resolve"
 import { terser } from "rollup-plugin-terser"
 import replace from "@rollup/plugin-replace"
@@ -11,16 +12,7 @@ const config = {
 const external = [
   ...Object.keys(pkg.dependencies || {}),
   ...Object.keys(pkg.peerDependencies || {}),
-  ...Object.keys(pkg.optionalDependencies || {}),
 ]
-
-const pureClass = {
-  transform(code) {
-    // Replace TS emitted @class function annotations with PURE so terser
-    // can remove them
-    return code.replace(/\/\*\* @class \*\//g, "/*@__PURE__*/")
-  },
-}
 
 const umd = Object.assign({}, config, {
   output: {
@@ -48,65 +40,85 @@ const umdProd = Object.assign({}, umd, {
     replace({
       "process.env.NODE_ENV": JSON.stringify("production"),
     }),
-    pureClass,
     terser({ output: { comments: false } }),
   ],
 })
 
-const cjs = Object.assign({}, config, {
-  output: {
-    file: `dist/${pkg.name}.cjs.js`,
-    format: "cjs",
-    exports: "named",
-  },
-  plugins: [resolve(), commonjs()],
-  external,
-})
+const distEntries = {
+  main: "lib/index.js",
+  react: "lib/react-entry.js",
+}
 
-const es = Object.assign({}, config, {
+const dist = {
+  input: distEntries,
+  output: ["cjs", "es"].map((format) => ({
+    dir: "dist",
+    format,
+    exports: "named",
+    entryFileNames: "[name].[format].js",
+    preserveModules: true,
+  })),
+  external,
+  plugins: [
+    {
+      name: "emit-proxy-package-jsons",
+      async buildEnd() {
+        const extraEntryNames = Object.keys(distEntries).filter(
+          (entyrName) => entyrName !== "main"
+        )
+
+        for (const entryName of extraEntryNames) {
+          try {
+            await fs.promises.mkdir(entryName)
+          } catch (err) {
+            if (!err || err.code !== "EEXIST") {
+              throw err
+            }
+          }
+
+          await fs.promises.writeFile(
+            `${entryName}/package.json`,
+            JSON.stringify(
+              {
+                private: true,
+                main: `../dist/${entryName}.cjs.js`,
+                module: `../dist/${entryName}.es.js`,
+                types: `../types/${entryName}-entry.d.ts`,
+              },
+              null,
+              2
+            ) + "\n"
+          )
+        }
+      },
+    },
+  ],
+}
+
+const createSizeBuild = ({ input, output }) => ({
+  input,
   output: {
     format: "es",
     exports: "named",
-    preserveModules: true,
-    dir: "dist/es",
+    file: output,
   },
-  plugins: [resolve(), commonjs()],
-  external,
+  plugins: [resolve(), terser({ output: { comments: false } })],
+  external: [...Object.keys(pkg.peerDependencies || {})],
 })
 
-const minPlugins = [resolve(), terser({ output: { comments: false } })]
-
-const sizeReact = Object.assign({}, es, {
+const sizeReact = createSizeBuild({
   input: "lib/targets/react/index.js",
-  output: Object.assign({}, es.output, {
-    file: `dist/size-react.js`,
-    preserveModules: false,
-    dir: undefined,
-  }),
-  plugins: minPlugins,
-  external: ["react", "react-dom"],
+  output: "dist/size-react.js",
 })
 
-const sizeAnimateDom = Object.assign({}, es, {
+const sizeAnimateDom = createSizeBuild({
   input: "lib/targets/dom/animate.js",
-  output: Object.assign({}, es.output, {
-    file: `dist/size-animate-dom.js`,
-    preserveModules: false,
-    dir: undefined,
-  }),
-  plugins: minPlugins,
-  external: ["react", "react-dom"],
+  output: "dist/size-animate-dom.js",
 })
 
-const sizeSpring = Object.assign({}, es, {
+const sizeSpring = createSizeBuild({
   input: "lib/generators/spring/index.js",
-  output: Object.assign({}, es.output, {
-    file: `dist/size-spring.js`,
-    preserveModules: false,
-    dir: undefined,
-  }),
-  plugins: minPlugins,
-  external: ["react", "react-dom"],
+  output: "dist/size-spring.js",
 })
 
-export default [umd, umdProd, cjs, es, sizeAnimateDom, sizeSpring, sizeReact]
+export default [dist, umd, umdProd, sizeAnimateDom, sizeSpring, sizeReact]
