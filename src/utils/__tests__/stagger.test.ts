368a2979c8e7037eb6030399b9b66ab91c760186
InventingWithMonster <mattgperry@gmail.com>
2021-08-16 12:05:00 +0200

    Adding stagger tests

--- src/utils/__tests__/stagger.test.ts
+++ src/utils/__tests__/stagger.test.ts
@@ -1,7 +1,28 @@
-import { getFromIndex } from "../stagger"
+import { stagger, getFromIndex } from "../stagger"
 
 describe("stagger", () => {
-  test("", () => {})
+  test("Creates a stagger function", () => {
+    expect(stagger(0.1)(0, 10)).toEqual(0)
+    expect(stagger(0.1)(2, 10)).toEqual(0.2)
+  })
+
+  test("Accepts start", () => {
+    expect(stagger(0.1, { start: 0.2 })(0, 10)).toEqual(0.2)
+    expect(stagger(0.1, { start: 0.2 })(2, 10)).toEqual(0.4)
+  })
+
+  test("Accepts from", () => {
+    expect(stagger(0.1, { from: 2 })(0, 10)).toEqual(0.2)
+    expect(stagger(0.1, { from: "first" })(2, 10)).toEqual(0.2)
+    expect(stagger(0.1, { from: "last" })(9, 10)).toEqual(0)
+    expect(stagger(0.1, { from: "last" })(5, 10)).toEqual(0.4)
+    expect(stagger(0.1, { from: "center" })(2, 10)).toEqual(0.25)
+  })
+
+  test("Accepts easing", () => {
+    expect(stagger(0.1, { easing: "linear" })(5, 10)).toEqual(0.5)
+    expect(stagger(0.1, { easing: (v: number) => v / 2 })(4, 10)).toEqual(0.2)
+  })
 })
 
 describe("getFromIndex", () => {
