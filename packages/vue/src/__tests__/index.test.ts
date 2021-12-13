4c1840d3db661ed530240eb0cd33630fea1b23c4
Matt Perry <mattgperry@gmail.com>
2021-12-13 20:43:25 +0100

    Updating vue tests

--- packages/vue/src/__tests__/index.test.ts
+++ packages/vue/src/__tests__/index.test.ts
@@ -11,22 +11,37 @@ describe("Motion", () => {
     expect(wrapper.get("[data-testid='box']").element).toBeTruthy()
   })
 
-  test("Renders element as 'as' prop", async () => {
+  test("Renders element as 'as' prop to HTML", async () => {
+    const wrapper = mount(Motion, {
+      props: { "data-testid": "box", as: "li" },
+    })
+    expect(wrapper.html()).toEqual(`<li data-testid="box"></li>`)
+  })
+
+  test("Renders element as 'as' prop to DOM node", async () => {
     const wrapper = mount(Motion, {
       props: { "data-testid": "box", as: "li" },
     })
     expect(wrapper.get("[data-testid='box']").element.tagName).toEqual("LI")
   })
 
-  test("Applies initial as style", async () => {
+  test("Applies initial as style to HTML", async () => {
     const wrapper = mount(Motion, {
       props: { "data-testid": "box", initial: { opacity: 0.5, x: 100 } },
     })
 
-    expect(wrapper.get("[data-testid='box']").element).toHaveStyle({
-      opacity: 0.5,
-      transform: "translateX(var(--motion-translateX))",
-      "--motion-translateX": "100px",
+    expect(wrapper.html()).toEqual(
+      `<div style="opacity: 0.5; --motion-translateX: 100px; transform: translateX(var(--motion-translateX));" data-testid="box"></div>`
+    )
+  })
+
+  test("Applies initial as style to DOM node", async () => {
+    const wrapper = mount(Motion, {
+      props: { "data-testid": "box", initial: { opacity: 0.5, x: 100 } },
     })
+
+    expect(wrapper.get("[data-testid='box']").attributes("style")).toEqual(
+      "opacity: 0.5; --motion-translateX: 100px; transform: translateX(var(--motion-translateX));"
+    )
   })
 })
