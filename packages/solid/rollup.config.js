c725db057c07377329c533c68172126c5014a85b
thetarnav <gthetarnav@gmail.com>
2022-04-18 14:52:49 +0200

    solid: cleanup rollup config

--- packages/solid/rollup.config.js
+++ packages/solid/rollup.config.js
@@ -1,4 +1,4 @@
-const { createDistBuild, createSizeBuild } = require("config/rollup.config")
+const { createSizeBuild } = require("config/rollup.config")
 const pkg = require("./package.json")
 const withSolid = require("rollup-preset-solid").default
 
@@ -6,7 +6,6 @@ module.exports = [
   withSolid({
     targets: ["esm", "cjs"],
     input: "src/index.ts",
-    printInstructions: true,
   }),
   createSizeBuild({ input: "dist/esm/index.js", output: "dist/size.js" }, pkg),
 ]
