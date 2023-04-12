468f0303c0b9ec53ee3bba3a35d4e491a0721292
productdevbook <hi@productdevbook.com>
2023-04-12 21:06:55 +0300

    feat: export dom type

--- packages/dom/src/easing/spring/index.ts
+++ packages/dom/src/easing/spring/index.ts
@@ -2,6 +2,7 @@ import {
   spring as createSpringGenerator,
   SpringOptions,
 } from "@motionone/generators"
+export type { SpringOptions }
 import { createGeneratorEasing } from "../create-generator-easing"
 
 export const spring = createGeneratorEasing<SpringOptions>(
