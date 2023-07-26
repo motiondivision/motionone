e02049966e69383d48e746bb81692b6acd5c62b3
Jason Quense <monastic.panic@gmail.com>
2023-07-26 08:58:57 -0700

    Update index.ts

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -63,6 +63,7 @@ export type Easing =
   | "steps-start"
   | "steps-end"
   | `steps(${number}, ${"start" | "end"})`
+  | `cubic-bezier(${string})`
   | BezierDefinition
 
 export type EasingGenerator = {
